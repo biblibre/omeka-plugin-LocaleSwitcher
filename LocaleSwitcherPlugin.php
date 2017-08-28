78f26162d6ba84d211762880889e52c65eab41f8
Julian Maurice <julian.maurice@biblibre.com>
2017-08-28 11:30:53 +0200

    Detect preferred language in Accept-Language HTTP header
    Also, do not allow languages that are not enabled in the config form

--- LocaleSwitcherPlugin.php
+++ LocaleSwitcherPlugin.php
@@ -63,13 +63,28 @@ class LocaleSwitcherPlugin extends Omeka_Plugin_AbstractPlugin
 
     public function filterLocale($value)
     {
+        $enabled_locales = unserialize(get_option('locale_switcher_locales'));
+
         // Make sure the session has been configured properly
         Zend_Registry::get('bootstrap')->bootstrap('Session');
 
         $session = new Zend_Session_Namespace('locale');
 
-        if ($session->locale) {
+        if ($session->locale && in_array($session->locale, $enabled_locales)) {
             return $session->locale;
+        }
+
+        if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+            $languages = array_map(function($l) {
+                list($locale, $q) = array_pad(explode(';', $l), 2, null);
+                return str_replace('-', '_', trim($locale));
+            }, explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']));
+
+            foreach ($languages as $language) {
+                if (in_array($language, $enabled_locales)) {
+                    return $language;
+                }
+            }
         }
     }
 }
